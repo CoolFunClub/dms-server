65330b8654ed4d0afd4de10412041ba85191b071
francisco laptop <serrfrancisco@yahoo.com>
2023-09-11 21:58:32 -0700

    first commit

---
+++ dms/src/test/java/com/coolfunclub/dms/DmsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.coolfunclub.dms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DmsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
