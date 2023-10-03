f8f7d32719bf6290c623558e3262a04f92e74101
Abanob Ayoub <abanobkamal369@gmail.com>
2023-10-03 12:03:26 -0700

    create Person.java

---
+++ dms/src/main/java/com/coolfunclub/dms/model/Person.java
@@ -0,0 +1,105 @@
+package com.coolfunclub.dms.model;
+
+import java.util.Random;
+
+public class Person {
+    private String mPersonID; //can be assigned
+    private String mFirstName;
+    private String mLastName;
+    private String mDateBirth;
+    private String mGender;
+    private String mPhone;
+    private String mEmail;
+    private String mAddress;
+
+    public Person(String personID, String firstName, String lastName,String dob, String gender, String phone, String email, String address){
+        this.mPersonID = personID;
+        this.mFirstName = firstName;
+        this.mLastName = lastName;
+        this.mDateBirth = dob;
+        this.mGender = gender;
+        this.mPhone = phone;
+        this.mEmail = email;
+        this.mAddress = address;
+    }
+
+    public Person(){ //Default Constructor
+    }
+
+       // Set methods
+       public void setSSN(String personID) {
+        this.mPersonID = personID;
+    }
+    public void setFirstName(String mFirstName) {
+        this.mFirstName = mFirstName;
+    }
+
+
+    public void setLastName(String mLastName) {
+        this.mLastName = mLastName;
+    }
+
+
+    public void setDateBirth(String mDateBirth) {
+        this.mDateBirth = mDateBirth;
+    }
+
+
+    public void setGender(String mGender) {
+        this.mGender = mGender;
+    }
+
+
+    public void setPhone(String mPhone) {
+        this.mPhone = mPhone;
+    }
+
+
+    public void setEmail(String mEmail) {
+        this.mEmail = mEmail;
+    }
+
+
+    public void setAddress(String mAddress) {
+        this.mAddress = mAddress;
+    }
+
+
+    // Get methods
+
+    public String getpersonID() {
+        return mPersonID;
+    }
+
+    public String getFirstName() {
+        return mFirstName;
+    }
+
+
+    public String getLastName() {
+        return mLastName;
+    }
+
+
+    public String getDateBirth() {
+        return mDateBirth;
+    }
+
+
+    public String getGender() {
+        return mGender;
+    }
+
+
+    public String getPhone() {
+        return mPhone;
+    }
+
+
+    public String getEmail() {
+        return mEmail;
+    }
+    public String getAddress() {
+        return mAddress;
+    }
+}
