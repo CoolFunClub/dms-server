3e83cc8e93b987c0ce986c2baace8c75fd4c459d
francisco laptop <serrfrancisco@yahoo.com>
2023-10-04 12:54:48 -0700

    moved tests/resovled imports

--- dms/src/test/java/com/coolfunclub/dms/test/CarTest.java
+++ dms/src/test/java/com/coolfunclub/dms/test/CarTest.java
@@ -14,6 +14,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import java.util.List;
 import java.util.LinkedList;
+
 import org.junit.jupiter.api.Test;
 import static org.assertj.core.api.Assertions.assertThat;
 
