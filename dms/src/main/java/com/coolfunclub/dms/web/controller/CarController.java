4ecaf895db14f54c1ae1437ff2765f53f1315863
francisco laptop <serrfrancisco@yahoo.com>
2023-10-04 21:28:06 -0700

    added cors annotation, react integration works on car controller

--- dms/src/main/java/com/coolfunclub/dms/web/controller/CarController.java
+++ dms/src/main/java/com/coolfunclub/dms/web/controller/CarController.java
@@ -3,16 +3,15 @@ package com.coolfunclub.dms.web.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
 import com.coolfunclub.dms.model.Car;
 import com.coolfunclub.dms.service.CarService;
 
+@CrossOrigin(origins = "http://localhost:3000")
 @RestController
 public class CarController {
 
