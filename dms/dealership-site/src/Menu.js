6c23f3f07095568b365400a350cc573f11336dd8
KaylaMLe <kaylale2014@gmail.com>
2023-10-07 09:39:53 -0700

    shaved down autogenerated files

--- dms/dealership-site/src/Menu.js
+++ dms/dealership-site/src/Menu.js
@@ -1,6 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './Menu.css';
 
 function App() {
   const [msg, setMsg] = useState('');
@@ -18,7 +17,6 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
           {msg}
         </p>
