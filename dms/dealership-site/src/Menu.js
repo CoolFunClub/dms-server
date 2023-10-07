1a26f8b5bccae0d9bda1b507c0ef1f78d23eb562
KaylaMLe <kaylale2014@gmail.com>
2023-10-07 10:23:44 -0700

    added basic layout

--- dms/dealership-site/src/Menu.js
+++ dms/dealership-site/src/Menu.js
@@ -1,26 +1,59 @@
 import React, { useState, useEffect } from 'react';
 import './Menu.css';
+import logo from "./assets/logo-placeholder.png";
+
+function LogoBar() {
+  return (
+    <div className="LogoBar">
+      <img src={logo} alt="Car dealership logo" />
+    </div>
+  );
+}
+
+function NavBarAndContent() {
+  return (
+    <div className="NavBarAndContent">
+      <NavBar />
+      <MainContent />
+    </div>
+  );
+}
+
+function NavBar() {
+  return (
+    <div className="NavBar">
+      <p>
+        buttons
+      </p>
+    </div>
+  );
+}
+
+function MainContent() {
+  return (
+    <div className="MainContent">
+      <p>Welcome!</p>
+    </div>
+  );
+}
 
 function App() {
-  const [msg, setMsg] = useState('');
+  // const [msg, setMsg] = useState('');
 
-  useEffect(() => {
-    const getMsg = async () => {
-      const data = await fetch("http://localhost:8080/cars");
-      const msgJson = await data.json();
-      console.log(msgJson);
-      setMsg(msgJson[0].vin);
-    }
-    getMsg();
-  }, []);
+  // useEffect(() => {
+  //   const getMsg = async () => {
+  //     const data = await fetch("http://localhost:8080/cars");
+  //     const msgJson = await data.json();
+  //     console.log(msgJson);
+  //     setMsg(msgJson[0].vin);
+  //   }
+  //   getMsg();
+  // }, []);
 
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          {msg}
-        </p>
-      </header>
+    <div>
+      <LogoBar />
+      <NavBarAndContent />
     </div>
   );
 }
