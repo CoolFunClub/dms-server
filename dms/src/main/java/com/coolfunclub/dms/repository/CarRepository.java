872ce3a7ae6be0228c5ffbd2017aa414665bf7c1
francisco laptop <serrfrancisco@yahoo.com>
2023-10-30 15:54:37 -0700

    hashed the pw

--- dms/src/main/java/com/coolfunclub/dms/repository/CarRepository.java
+++
@@ -1,7 +0,0 @@
-package com.coolfunclub.dms.repository;
-import org.springframework.data.repository.CrudRepository;
-import com.coolfunclub.dms.model.Car;
-
-public interface CarRepository extends CrudRepository<Car, String> {
-    
-}
