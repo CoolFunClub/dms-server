80015bb4ba71191b4450bbb148e1c5a2de0f5d7b
KaylaMLe <kaylale2014@gmail.com>
2024-01-14 00:37:09 +0000

    using test values to try to run the app on port 8443

--- src/main/java/com/coolfunclub/dms/DmsApplication.java
+++ src/main/java/com/coolfunclub/dms/DmsApplication.java
@@ -4,6 +4,8 @@ import java.util.*;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.filter.CorsFilter;
@@ -26,4 +28,9 @@ public class DmsApplication {
 		source.registerCorsConfiguration("/**", config);
 		return new CorsFilter(source);
 	}
+}
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
 }
